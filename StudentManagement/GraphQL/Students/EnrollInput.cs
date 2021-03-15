126887ff0620dc8107ae1a0565fe4f6f6b7c7fb8
bds0900 <Dbeak9336@conestogac.on.ca>
2021-03-14 20:42:43 -0400

    add a query for student enroll in a course

---
+++ StudentManagement/GraphQL/Students/EnrollInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.GraphQL.Students
+{
+    public record EnrollInput(
+        string StudentNumber,
+        string CourseNumber
+    );
+    
+}
